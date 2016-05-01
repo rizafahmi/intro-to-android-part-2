db13fb3d9d6b4e117d5507ee811fa49bd8a485e3
Riza Fahmi <rizafahmi@gmail.com>
2016-05-01 22:12:22 +0700

    Refactor #1: Add FactBook class

--- app/src/main/java/com/elixirdose/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/elixirdose/funfacts/FunFactsActivity.java
@@ -13,6 +13,8 @@ public class FunFactsActivity extends AppCompatActivity {
     private Button mButton;
     private TextView mTextView;
 
+    private FactBook mFactBook = new FactBook();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,26 +26,9 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built." };
-
-                // The button was clicked, so update the fact TextView with a new fact
-                String fact = "";
-                // Randomly select a fact
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                fact = facts[randomNumber];
 
                 //Update the screen with our dynamic fact
+                String fact = mFactBook.getFact();
                 mTextView.setText(fact);
             }
         };
