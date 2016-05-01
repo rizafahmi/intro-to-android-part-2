2ea0a06a322330eb88cb0825b8876e1513ec0ef1
Riza Fahmi <rizafahmi@gmail.com>
2016-05-01 19:18:22 +0700

    Define the elements

--- app/src/main/java/com/elixirdose/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/elixirdose/funfacts/FunFactsActivity.java
@@ -2,12 +2,20 @@ package com.elixirdose.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        mButton = (Button) findViewById(R.id.button);
+        mTextView = (TextView) findViewById(R.id.textView);
     }
 }
